5a5ac8b4130ba5ff5f57e093171f42ad4cf0ffc0
Jiaxi Wu <jiaxwu159@gmail.com>
2019-01-16 23:21:48 -0500

    finished counter, can reset, and pause

--- src/components/Thunderbutton/Thunderbutton.js
+++ src/components/Thunderbutton/Thunderbutton.js
@@ -1,3 +1,4 @@
+
 import React from'react'
 //import img from './img.svg';
 
@@ -5,23 +6,45 @@ import React from'react'
 const Thunderbutton = () => {
 	return (
 		<div className='bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5'>
-		<button id="home_button" onClick={clickMe}></button>
+		<button id="home_button" onClick={lightning}></button>
 		</div>
 )
 };
 
 
 let x = 0
+let myInterval = -1
 
-function clickMe(){
-	setInterval(lightning, 1000);
+function reset() {
+	x = 0
 }
 
 function lightning(){
-
-	let displayNumbers = x++
-	console.log(displayNumbers)
+ if (myInterval === -1) {
+ 	myInterval = setInterval(function(){
+ 		let displayNumbers = x++
+		console.log(displayNumbers)}
+, 1000)
+ 	return(myInterval)
+ }
+	else {
+	clearInterval(myInterval);
+	myInterval = -1;
 }
+
+ if (myInterval === 0){
+ 		alert("storms here");
+ 	}
+ 	else if (myInterval >= 1 && myInterval <=3) {
+ 		alert ("storms not too far off")
+ 	}
+ 	else {
+ 		alert ("storms far away")
+ 	}
+	reset()
+};
+
+
 //className='bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5'
  //<img src={img} alt='thunder' width='300' height='300' />
 
